8eb020f6d509871039ab31e3f97cf388c490d0c2
LucyZheng <37244661+LucyZheng@users.noreply.github.com>
2019-04-16 11:15:56 +0800

    补之前的上传

---
+++ 16计科+16337327+郑映雪+HW5/代码/homework5.cpp
@@ -0,0 +1,49 @@
+#include <GL/freeglut.h>
+#include <stdlib.h>
+void display(void){
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glutSolidTeapot(0.5);
+	glFlush();  
+}
+
+void reshape(int w, int h){
+	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	//正交投影
+	if (w <= h)
+		glOrtho(-1.5, 1.5, -1.5*(GLfloat)h / (GLfloat)w, 1.5*(GLfloat)h / (GLfloat)w, -10.0, 10.0);
+	else
+		glOrtho(-1.5*(GLfloat)w / (GLfloat)h, 1.5*(GLfloat)w / (GLfloat)h, -1.5, 1.5, -10.0, 10.0);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
+
+int main(int argc, char** argv){
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitWindowSize(500, 500);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow("homework 5");
+	GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };  //镜面反射参数
+	GLfloat mat_shininess[] = { 50.0 };               //高光指数
+	GLfloat light_position[] = { 1.0, 1.0, 1.0, 0.0 };
+	GLfloat white_light[] = { 1.0, 1.0, 1.0, 1.0 };   //灯位置(1,1,1), 最后1-开关
+	GLfloat Light_Model_Ambient[] = { 0.2, 0.2, 0.2, 1.0 }; //环境光参数
+	glClearColor(0.0, 0.0, 0.0, 0.0);  
+	glShadeModel(GL_SMOOTH);           //多变性填充模式					  
+	glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+	glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
+	glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, white_light);   //散射光属性
+	glLightfv(GL_LIGHT0, GL_SPECULAR, white_light);  //镜面反射光
+	glLightModelfv(GL_LIGHT_MODEL_AMBIENT, Light_Model_Ambient);  //环境光参数
+	glEnable(GL_LIGHTING);   //开关:使用光
+	glEnable(GL_LIGHT0);     //打开0#灯
+	glEnable(GL_DEPTH_TEST); //打开深度测试
+	glutDisplayFunc(display);
+	glutReshapeFunc(reshape);
+
+	glutMainLoop();
+	return 0;
+}
